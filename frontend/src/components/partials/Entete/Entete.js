7f8c84550ccde387790bb34ac874e53e7c71617e
Clara Quintela <claraquintela@gmail.com>
2024-06-20 14:57:29 -0400

    Re-installer la bibliothèque i18n

--- frontend/src/components/partials/Entete/Entete.js
+++ frontend/src/components/partials/Entete/Entete.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import "./Entete.css";
+import Nav from '../Nav/Nav';
 
-function Entete({ t}) {
+function Entete({t}) {
    
 
     return (
         <header>
+
             <h1>{t("titre")}</h1>
+            <Nav/>
         </header>
     );
 }
