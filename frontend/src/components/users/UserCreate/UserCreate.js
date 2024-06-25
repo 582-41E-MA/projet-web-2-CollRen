98508b463a900fe35e14cd711fc0cc21ad5473b2
Clara Quintela <claraquintela@gmail.com>
2024-06-25 11:39:35 -0400

    User Create working! (tâche 35 fini)
    Maintenant c'est possible d'ajouter un utilisateur. Son privilège par default est 3 (client)

--- frontend/src/components/users/UserCreate/UserCreate.js
+++ frontend/src/components/users/UserCreate/UserCreate.js
@@ -2,59 +2,94 @@ import Bouton from '../../partialsFormulaire/Bouton/Bouton';
 import {useState} from 'react';
 import ChampText from '../../partialsFormulaire/ChampText/ChampText';
 
-
 function UserCreate ({t}) {
 
-    const [nom, setNom] = useState('')
-    const [courriel, setCourriel] = useState('')    
-    const [mdp, setMdp] = useState('')
-    const [utilisateur, setUtilisateurs] = useState("")
+    const [prenom, setPrenom] = useState('');
+    const [nom, setNom] = useState('');
+    const [courriel, setCourriel] = useState('');    
+    const [mdp, setMdp] = useState('');
+    const [privilegeId, setPrivilegeId] = useState('');
 
+    function createUser (e) {
+        e.preventDefault();
+        console.log("create user");
 
-   function createUser (e) {
-    e.preventDefault();
-    console.log("create user");
+        const newUser = {
+            prenom: prenom,
+            nom: nom,
+            courriel: courriel,
+            mdp: mdp,
+            privilege_id: 3
+        };
 
-     //Clear form
-     e.target.reset();
+        fetch('http://localhost:5000/api/utilisateurs', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newUser)
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            setNom('');
+            setCourriel('');
+            setMdp('');
+            setPrivilegeId('');
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+
+        //Clear form
+        e.target.reset();
     }
-  
-    return (
 
+    return (
         <main className="container mx-auto p-4 mt-12 bg-white flex flex-col items-center justify-center text-gray-700">
             <div className="w-10/12 sm:w-8/12 md:w-6/12 lg:w-5/12 xl:w-4/12 mb-4">
-                <h2 className="py-6"> {t("CreateUser.soustitre")}</h2>
+                <h2 className="py-6">{t("CreateUser.soustitre")}</h2>
             </div>
 
             <form onSubmit={createUser} className="w-10/12 sm:w-8/12 md:w-6/12 lg:w-5/12 xl:w-4/12 mb-6">
-                
+
+                <ChampText
+                    mandatory={true} 
+                    label={t("CreateUser.prenomLabel")}
+                    type="text"
+                    name="prenom"
+                    placeholder={t("CreateUser.prenomPlaceHolder")}
+                    content={nom} 
+                    whenChanged={setNom}/>
 
                 <ChampText
                     mandatory={true} 
                     label={t("CreateUser.nomLabel")}
+                    type="text"
                     name="nom"
                     placeholder={t("CreateUser.nomPlaceHolder")}
                     content={nom} 
-                    whenChanged={nom => setNom(nom)}/>
+                    whenChanged={setNom}/>
 
                 <ChampText
                     mandatory={true} 
                     label={t("CreateUser.courrielLabel")}
+                    type="email"
                     name="courriel"
                     placeholder={t("CreateUser.courrielPlaceHolder")}
                     content={courriel} 
-                    whenChanged={ content => setCourriel(content)}/>
+                    whenChanged={setCourriel}/>
                         
-                    <ChampText
+                <ChampText
                     mandatory={true} 
                     label={t("CreateUser.mdpLabel")}
+                    type="password"
                     name="mdp"
                     placeholder={t("CreateUser.mdpLabel")}
                     content={mdp} 
-                    whenChanged={ content => setMdp(content)}/>
-                          
+                    whenChanged={setMdp}/>
+                   
 
-                    
                 <Bouton type="submit">
                     {t("CreateUser.btnSubmit")}
                 </Bouton>
@@ -63,4 +98,4 @@ function UserCreate ({t}) {
     )
 }
 
-export default UserCreate ;    	
+export default UserCreate;
