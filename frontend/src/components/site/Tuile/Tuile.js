32df4bc34f5d5221e0d94f0e71845b6d76b7d8a3
NabilDjerroud <nabil.djerroud93@hotmail.com>
2024-07-09 15:36:54 -0400

    accepter les changement sur Tuile

--- frontend/src/components/site/Tuile/Tuile.js
+++ frontend/src/components/site/Tuile/Tuile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next'; 
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next'; 
 
 function Tuile({ voiture, language }) {
     const principaleImage = voiture.images.find(image => image.est_principale === 1 && image.voiture_id === voiture.id);
