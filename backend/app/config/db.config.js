a167423d5a069acc489db6f29d071a417726570a
NabilDjerroud <nabil.djerroud93@hotmail.com>
2024-07-09 14:29:17 -0400

    résoudre conflit

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,10 +1,13 @@
 module.exports = {
 
+<<<<<<< HEAD
     // PORT: 3306,
     // PASSWORD: "",
     // DB: "bobazou",
 
     // PORT: 3308,
+=======
+>>>>>>> c5166fc (corriger l'affichage de la page usercreaete)
     PORT: 3306,
     PASSWORD: "",
     DB: "bobazou",
