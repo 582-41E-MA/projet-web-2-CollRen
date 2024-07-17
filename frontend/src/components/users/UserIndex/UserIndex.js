7a704a25a4c531eaa3acb5c203f938c8bb4a0f35
Clara Quintela <claraquintela@gmail.com>
2024-07-17 18:04:19 -0400

    Ajouter ville et province à utilisateurs (front)
    Maintenant sur utilisateursIndex l 'admin peut acceder aux info sur la ville et la province

--- frontend/src/components/users/UserIndex/UserIndex.js
+++ frontend/src/components/users/UserIndex/UserIndex.js
@@ -6,15 +6,48 @@ import Bouton from '../../partialsFormulaire/Bouton/Bouton';
 function UserIndex({ t }) {
     const [users, setUsers] = useState([]);
     const [sortConfig, setSortConfig] = useState({ key: null, direction: 'asc' });
+    const [language, setLanguage] = useState(localStorage.getItem('langueChoisie') || 'en');
 
     useEffect(() => {
-        fetch(`${t("fetch")}utilisateurs`)
-            .then(response => response.json())
-            .then(data => {
-                setUsers(data);
-            })
-            .catch(error => console.error('Error fetching users:', error));
-    }, []);
+        const fetchUsers = async () => {
+            try {
+                const response = await fetch(`${t("fetch")}utilisateurs`);
+                const usersData = await response.json();
+
+                const fetchCityAndProvince = async (user) => {
+                    try {
+                        const cityResponse = await fetch(`${t("fetch")}villes/${user.ville_id}`);
+                        const cityData = await cityResponse.json();
+                        const provinceResponse = await fetch(`${t("fetch")}provinces/${cityData.province_id}`);
+                        const provinceData = await provinceResponse.json();
+
+                        return {
+                            ...user,
+                            privilege: {
+                                ...user.privilege,
+                                type: JSON.parse(user.privilege.type)
+                            },
+                            ville: {
+                                ...cityData,
+                                nom: JSON.parse(cityData.nom),
+                                province: JSON.parse(provinceData.nom)
+                            }
+                        };
+                    } catch (error) {
+                        console.error('Error fetching city or province:', error);
+                        return user;
+                    }
+                };
+
+                const updatedUsers = await Promise.all(usersData.map(fetchCityAndProvince));
+                setUsers(updatedUsers);
+            } catch (error) {
+                console.error('Error fetching users:', error);
+            }
+        };
+
+        fetchUsers();
+    }, [t]);
 
     const handleSort = (key) => {
         let direction = 'asc';
@@ -23,7 +56,7 @@ function UserIndex({ t }) {
         }
         setSortConfig({ key, direction });
     };
-
+    
     const sortedUsers = [...users].sort((a, b) => {
         if (sortConfig.key) {
             if (sortConfig.key === 'privilege_id') {
@@ -31,6 +64,22 @@ function UserIndex({ t }) {
                     return a[sortConfig.key] - b[sortConfig.key];
                 } else {
                     return b[sortConfig.key] - a[sortConfig.key];
+                }
+            } else if (sortConfig.key === 'ville') {
+                const aValue = a.ville.nom[language];
+                const bValue = b.ville.nom[language];
+                if (sortConfig.direction === 'asc') {
+                    return aValue.localeCompare(bValue);
+                } else {
+                    return bValue.localeCompare(aValue);
+                }
+            } else if (sortConfig.key === 'province') {
+                const aValue = a.ville.province ? a.ville.province[language] : '';
+                const bValue = b.ville.province ? b.ville.province[language] : '';
+                if (sortConfig.direction === 'asc') {
+                    return aValue.localeCompare(bValue);
+                } else {
+                    return bValue.localeCompare(aValue);
                 }
             } else {
                 if (sortConfig.direction === 'asc') {
@@ -42,6 +91,7 @@ function UserIndex({ t }) {
         }
         return 0;
     });
+    
 
     return (
         <div className="flex">
@@ -50,46 +100,57 @@ function UserIndex({ t }) {
             </div>
             <div className='flex flex-col w-[40%] mx-[3rem] mt-24 mb-[4rem]'>
 
-                <h2 className="mx-[4rem] p-3">{t("user_list_titre")}</h2>
+                <h2 className="mb-[2rem] p-3">{t("user_list_titre")}</h2>
 
                 <table className="w-full divide-y divide-gray-200 bg-[#21283B] mt-4 rounded-lg mb-[4rem]">
-                    <thead>
-                        <tr>
-                            <th
-                                className="px-6 py-3 text-left text-xs font-large text-gray-500 uppercase tracking-wider cursor-pointer"
-                                onClick={() => handleSort('prenom')}
-                            >
-                                <span className={sortConfig.key === 'prenom' ? 'font-bold' : ''}>
-                                    {t("user.prenom")} {sortConfig.key === 'prenom' ? (sortConfig.direction === 'asc' ? '▲' : '▼') : '⇅'}
-                                </span>
-                            </th>
-                            <th
-                               className="px-6 py-3 text-left text-xs font-large text-gray-500 uppercase tracking-wider cursor-pointer"
-                                onClick={() => handleSort('nom')}
-                            >
-                                <span className={sortConfig.key === 'nom' ? 'font-bold' : ''}>
-                                    {t("user.nom")} {sortConfig.key === 'nom' ? (sortConfig.direction === 'asc' ? '▲' : '▼') : '⇅'}
-                                </span>
-                            </th>
-                            <th className="px-6 py-3 text-left text-xs font-large text-gray-500 uppercase tracking-wider">{t("user.anniversaire")}</th>
-                            <th className="px-6 py-3 text-left text-xs font-large text-gray-500 uppercase tracking-wider">{t("user.courriel")}</th>
-                            <th className="px-6 py-3 text-left text-xs font-large text-gray-500 uppercase tracking-wider">{t("user.adresse")}</th>
-                            <th className="px-6 py-3 text-left text-xs font-large text-gray-500 uppercase tracking-wider">{t("user.codepostal")}</th>
-                            <th className="px-6 py-3 text-left text-xs font-large text-gray-500 uppercase tracking-wider">{t("user.ville")}</th>
-                            <th className="px-6 py-3 text-left text-xs font-large text-gray-500 uppercase tracking-wider">{t("user.province")}</th>
-                            <th className="px-6 py-3 text-left text-xs font-large text-gray-500 uppercase tracking-wider">{t("user.telephone")}</th>
-                            <th className="px-6 py-3 text-left text-xs font-large text-gray-500 uppercase tracking-wider">{t("user.cellulaire")}</th>
-                            <th
-                                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider cursor-pointer"
-                                onClick={() => handleSort('privilege_id')}
-                            >
-                                <span className={sortConfig.key === 'privilege_id' ? 'font-bold' : ''}>
-                                    {t("user.privilege")} {sortConfig.key === 'privilege_id' ? (sortConfig.direction === 'asc' ? '▲' : '▼') : '⇅'}
-                                </span>
-                            </th>
-                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Actions</th>
-                        </tr>
-                    </thead>
+                <thead>
+    <tr>
+        <th
+            className="px-6 py-3 text-left text-xs font-large text-gray-500 uppercase tracking-wider cursor-pointer"
+            onClick={() => handleSort('prenom')}
+        >
+            <span className={sortConfig.key === 'prenom' ? 'font-bold' : ''}>
+                {t("user.prenom")} {sortConfig.key === 'prenom' ? (sortConfig.direction === 'asc' ? '▲' : '▼') : '⇅'}
+            </span>
+        </th>
+        <th
+           className="px-6 py-3 text-left text-xs font-large text-gray-500 uppercase tracking-wider cursor-pointer"
+            onClick={() => handleSort('nom')}
+        >
+            <span className={sortConfig.key === 'nom' ? 'font-bold' : ''}>
+                {t("user.nom")} {sortConfig.key === 'nom' ? (sortConfig.direction === 'asc' ? '▲' : '▼') : '⇅'}
+            </span>
+        </th>
+        <th className="px-6 py-3 text-left text-xs font-large text-gray-500 uppercase tracking-wider">{t("user.anniversaire")}</th>
+        <th className="px-6 py-3 text-left text-xs font-large text-gray-500 uppercase tracking-wider">{t("user.courriel")}</th>
+        <th
+           className="px-6 py-3 text-left text-xs font-large text-gray-500 uppercase tracking-wider cursor-pointer"
+           onClick={() => handleSort('ville')}
+        >
+            <span className={sortConfig.key === 'ville' ? 'font-bold' : ''}>
+                {t("user.ville")} {sortConfig.key === 'ville' ? (sortConfig.direction === 'asc' ? '▲' : '▼') : '⇅'}
+            </span>
+        </th>
+        <th
+           className="px-6 py-3 text-left text-xs font-large text-gray-500 uppercase tracking-wider cursor-pointer"
+           onClick={() => handleSort('province')}
+        >
+            <span className={sortConfig.key === 'province' ? 'font-bold' : ''}>
+                {t("user.province")} {sortConfig.key === 'province' ? (sortConfig.direction === 'asc' ? '▲' : '▼') : '⇅'}
+            </span>
+        </th>
+        <th
+            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider cursor-pointer"
+            onClick={() => handleSort('privilege_id')}
+        >
+            <span className={sortConfig.key === 'privilege_id' ? 'font-bold' : ''}>
+                {t("user.privilege")} {sortConfig.key === 'privilege_id' ? (sortConfig.direction === 'asc' ? '▲' : '▼') : '⇅'}
+            </span>
+        </th>
+        <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Actions</th>
+    </tr>
+</thead>
+
                     <tbody className="bg-[#21283B] divide-y divide-gray-200">
                         {sortedUsers.map((user, index) => (
                             <tr key={index}>
@@ -97,13 +158,10 @@ function UserIndex({ t }) {
                                 <td className="px-4 py-2 whitespace-nowrap text-white">{user.nom}</td>
                                 <td className="px-4 py-2 whitespace-nowrap text-white">{user.anniversaire}</td>
                                 <td className="px-4 py-2 whitespace-nowrap text-white">{user.courriel}</td>
-                                <td className="px-4 py-2 whitespace-nowrap text-white">{user.adresse}</td>
-                                <td className="px-4 py-2 whitespace-nowrap text-white">{user.code_postal}</td>
-                                <td className="px-4 py-2 whitespace-nowrap text-white">{user.ville}</td>
-                                <td className="px-4 py-2 whitespace-nowrap text-white">{user.province}</td>
-                                <td className="px-4 py-2 whitespace-nowrap text-white">{user.telephone}</td>
-                                <td className="px-4 py-2 whitespace-nowrap text-white">{user.cellulaire}</td>
-                                <td className="px-4 py-2 whitespace-nowrap text-white">{user.privilege_id}</td>
+                               
+                                <td className="px-4 py-2 whitespace-nowrap text-white">{user.ville.nom[language]}</td>
+                                <td className="px-4 py-2 whitespace-nowrap text-white">{user.ville.province ? user.ville.province[language] : ''}</td>
+                                <td className="px-4 py-2 whitespace-nowrap text-white">{user.privilege.type[language]}</td>
                                 <td className="px-4 py-2 whitespace-nowrap text-white">
                                     <Link to={`/user/${user.id}`} >
                                         <Bouton>{t("btnEditer")}</Bouton>
