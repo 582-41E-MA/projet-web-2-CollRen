79458dbeae507e82e66629c5a732fa7387492e94
Clara Quintela <claraquintela@gmail.com>
2024-07-17 16:25:01 -0400

    Implementation BarreRecherche sur VoituresIndex
    pour arriver a ça, j'ai changer tous les components reliés à la barra de recherche

--- frontend/src/components/site/BarreRecherche/UneVoiture.js
+++ frontend/src/components/site/BarreRecherche/UneVoiture.js
@@ -4,21 +4,23 @@ function UneVoiture(props) {
     let t = props.t
     let voiture = props.voiture;
     let language = props.language;
+    let location = props.location;
 
     return (
 
         <tr key={props?.id}>
-
-            <td className="px-2 py-2 whitespace-nowrap text-white">{voiture?.date}</td>
-            <td className="px-2 py-2 whitespace-nowrap text-white">{voiture.modele.constructeur?.type}</td>
-            <td className="px-2 py-2 whitespace-nowrap text-white">{voiture.modele?.type}</td>
-            <td className="px-2 py-2 whitespace-nowrap">
-                <Link to={`/voitures/${voiture.id}`} className="gap-x-[1rem] bg-[#F96C25] hover:bg-[#868E9B] text-white font-bold py-2 px-4 rounded-full">
-                    {t("btnChoisir")}
-                </Link>
-
-            </td>
-        </tr>
+        <td className="px-2 py-2 whitespace-nowrap text-white">{voiture?.date}</td>
+        <td className="px-2 py-2 whitespace-nowrap text-white">{voiture.modele.constructeur?.type}</td>
+        <td className="px-2 py-2 whitespace-nowrap text-white">{voiture.modele?.type}</td>
+        <td className="px-2 py-2 whitespace-nowrap">
+            <Link 
+                to={location === "/" ? `/voitures/${voiture.id}` : `/voiture-update/${voiture.id}`} 
+                className="gap-x-[1rem] bg-[#F96C25] hover:bg-[#868E9B] text-white font-bold py-2 px-4 rounded-full">
+                {t("btnChoisir")}
+            </Link>
+        </td>
+    </tr>
+    
 
     )
 
