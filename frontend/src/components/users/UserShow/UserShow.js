1ab557816dace413b67915c1410f29185f08cded
Clara Quintela <claraquintela@gmail.com>
2024-07-18 16:52:10 -0400

    Corriger le component UserShow
    Ameliorer les selects provinces et villes

--- frontend/src/components/users/UserShow/UserShow.js
+++ frontend/src/components/users/UserShow/UserShow.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import ChampText from '../../partialsFormulaire/ChampText/ChampText';
+import MenuDashboardAdmin from '../../dashboards/MenuDashboardAdmin/MenuDashboardAdmin';
+import Bouton from '../../partialsFormulaire/Bouton/Bouton';
 
 function UserShow({ t }) {
   const { id } = useParams();
@@ -14,11 +16,14 @@ function UserShow({ t }) {
     telephone: '',
     cellulaire: '',
     ville_id: '',
-    province_id: ''
+    province_id: '',
+    nom_utilisateur: '',
+    privilege_type: '' // Assuming you will get this from the API response
   });
 
   const [villes, setVilles] = useState([]);
   const [provinces, setProvinces] = useState([]);
+  const [privileges, setPrivileges] = useState([]);
   const [language, setLanguage] = useState(localStorage.getItem('langueChoisie'));
 
   useEffect(() => {
@@ -28,7 +33,8 @@ function UserShow({ t }) {
       .then(data => {
         setUser({
           ...data,
-          province_id: data.ville.province_id // Assumindo que a API retorna isso
+          province_id: data.ville.province_id, // Assumindo que a API retorna isso
+          privilege_type: JSON.parse(data.privilege.type)[language] // Transforming string into object
         });
       })
       .catch(error => {
@@ -63,7 +69,21 @@ function UserShow({ t }) {
         console.error("There was an error fetching the provinces!", error);
       });
 
-  }, [id, t]);
+    // Fetch privileges data
+    fetch(`${t("fetch")}privileges`)
+      .then(response => response.json())
+      .then(data => {
+        const updatedData = data.map(item => ({
+          ...item,
+          type: JSON.parse(item.type)
+        }));
+        setPrivileges(updatedData);
+      })
+      .catch(error => {
+        console.error("There was an error fetching the privileges!", error);
+      });
+
+  }, [id, t, language]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -71,6 +91,21 @@ function UserShow({ t }) {
       ...prevState,
       [name]: value
     }));
+
+    if (name === 'province_id') {
+      setUser(prevState => ({
+        ...prevState,
+        ville_id: '' // Reset ville when province changes
+      }));
+    } else if (name === 'ville_id') {
+      const selectedVille = villes.find(ville => ville.id === parseInt(value));
+      if (selectedVille) {
+        setUser(prevState => ({
+          ...prevState,
+          province_id: selectedVille.province_id // Update province when ville changes
+        }));
+      }
+    }
   };
 
   const handleUpdate = (e) => {
@@ -104,15 +139,21 @@ function UserShow({ t }) {
       });
   };
 
+  const filteredVilles = villes.filter(ville => ville.province_id === parseInt(user.province_id));
+
   return (
     <div className="flex">
-        
+        <div>
+            <MenuDashboardAdmin t={t}/>
+        </div>
     
-      <div className="w-[65%] mx-[4rem] mt-24">
-        <form className="max-w-lg mx-auto bg-white p-8 rounded-md shadow-md" onSubmit={handleUpdate}>
+      <div className="flex flex-col w-full mx-[3rem] mt-24 mb-[4rem] ">
+
+        <h1 className='text-[#21283B] text-left mb-[2rem]'>{t("user.EditTitle")}</h1>
+        <form className="w-[65%] mx-0 bg-[#21283B] p-8 rounded-md shadow-md mb-[4rem]" onSubmit={handleUpdate}>
           <div className="grid grid-cols-1 gap-6">
             <div>
-              <label className="block text-sm font-medium text-gray-700">{t("user.prenom")}</label>
+              <label className="block text-sm text-white font-medium ">{t("user.prenom")}</label>
               <ChampText
                 type="text"
                 name="prenom"
@@ -122,7 +163,7 @@ function UserShow({ t }) {
               />
             </div>
             <div>
-              <label className="block text-sm font-medium text-gray-700">{t("user.nom")}</label>
+              <label className="block text-sm text-white font-medium ">{t("user.nom")}</label>
               <ChampText
                 type="text"
                 name="nom"
@@ -132,7 +173,17 @@ function UserShow({ t }) {
               />
             </div>
             <div>
-              <label className="block text-sm font-medium text-gray-700">{t("user.anniversaire")}</label>
+              <label className="block text-sm font-medium text-white">{t("user.nom_utilisateur")}</label>
+              <ChampText
+                type="text"
+                name="nom_utilisateur"
+                value={user.nom_utilisateur}
+                onChange={handleChange}
+                className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-white">{t("user.anniversaire")}</label>
               <ChampText
                 type="date"
                 name="anniversaire"
@@ -142,7 +193,7 @@ function UserShow({ t }) {
               />
             </div>
             <div>
-              <label className="block text-sm font-medium text-gray-700">{t("user.courriel")}</label>
+              <label className="block text-sm font-medium text-white">{t("user.courriel")}</label>
               <ChampText
                 type="email"
                 name="courriel"
@@ -152,7 +203,7 @@ function UserShow({ t }) {
               />
             </div>
             <div>
-              <label className="block text-sm font-medium text-gray-700">{t("user.adresse")}</label>
+              <label className="block text-sm font-medium text-white">{t("user.adresse")}</label>
               <ChampText
                 type="text"
                 name="adresse"
@@ -162,7 +213,7 @@ function UserShow({ t }) {
               />
             </div>
             <div>
-              <label className="block text-sm font-medium text-gray-700">{t("user.codepostal")}</label>
+              <label className="block text-sm font-medium text-white">{t("user.codepostal")}</label>
               <ChampText
                 type="text"
                 name="code_postal"
@@ -172,7 +223,7 @@ function UserShow({ t }) {
               />
             </div>
             <div>
-              <label className="block text-sm font-medium text-gray-700">{t("user.telephone")}</label>
+              <label className="block text-sm font-medium text-white">{t("user.telephone")}</label>
               <ChampText
                 type="text"
                 name="telephone"
@@ -182,7 +233,7 @@ function UserShow({ t }) {
               />
             </div>
             <div>
-              <label className="block text-sm font-medium text-gray-700">{t("user.cellulaire")}</label>
+              <label className="block text-sm font-medium text-white">{t("user.cellulaire")}</label>
               <ChampText
                 type="text"
                 name="cellulaire"
@@ -192,23 +243,7 @@ function UserShow({ t }) {
               />
             </div>
             <div>
-              <label className="block text-sm font-medium text-gray-700">{t("user.ville")}</label>
-              <select
-                name="ville_id"
-                value={user.ville_id}
-                onChange={handleChange}
-                className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-              >
-                <option value="">{t("user.selectville")}</option>
-                {villes.map(ville => (
-                  <option key={ville.id} value={ville.id}>
-                    {ville.nom[language]}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div>
-              <label className="block text-sm font-medium text-gray-700">{t("user.province")}</label>
+              <label className="block text-sm font-medium text-white">{t("user.province")}</label>
               <select
                 name="province_id"
                 value={user.province_id}
@@ -223,21 +258,53 @@ function UserShow({ t }) {
                 ))}
               </select>
             </div>
+            <div>
+              <label className="block text-sm font-medium text-white">{t("user.ville")}</label>
+              <select
+                name="ville_id"
+                value={user.ville_id}
+                onChange={handleChange}
+                className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              >
+                <option value="">{t("user.selectville")}</option>
+                {filteredVilles.map(ville => (
+                  <option key={ville.id} value={ville.id}>
+                    {ville.nom[language]}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-white">{t("user.privilege")}</label>
+              <select
+                name="privilege_type"
+                value={user.privilege_type}
+                onChange={handleChange}
+                className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              >
+                <option value="">{t("user.selectprivilege")}</option>
+                {privileges.map(privilege => (
+                  <option key={privilege.id} value={privilege.id}>
+                    {privilege.type[language]}
+                  </option>
+                ))}
+              </select>
+            </div>
           </div>
           <div className="mt-6 flex justify-end">
-            <button
+            <Bouton
               type="submit"
               className="px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out"
             >
               Update
-            </button>
-            <button
+            </Bouton>
+            <Bouton
               type="button"
               onClick={handleDelete}
               className="ml-2 px-4 py-2 font-medium text-white bg-red-600 rounded-md hover:bg-red-500 focus:outline-none focus:shadow-outline-red active:bg-red-600 transition duration-150 ease-in-out"
             >
               Delete
-            </button>
+            </Bouton>
           </div>
         </form>
       </div>
