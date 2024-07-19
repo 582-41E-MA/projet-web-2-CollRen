7b1eaefc2796a79a2ee86c77bfd1dce38f34fdc1
NabilDjerroud <nabil.djerroud93@hotmail.com>
2024-07-18 23:17:45 -0400

    integrer dans le formulaire de paiement

--- frontend/src/components/vente/FormFacture.js
+++ frontend/src/components/vente/FormFacture.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements, CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
 import ChampText from '../partialsFormulaire/ChampText/ChampText';
 import Bouton from '../partialsFormulaire/Bouton/Bouton';
+
+// Charger Stripe avec votre clé publique
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
 
 function FormFacture({ t, userId, totalPanier }) {
   const [user, setUser] = useState({
@@ -27,11 +32,9 @@ function FormFacture({ t, userId, totalPanier }) {
 
   useEffect(() => {
     if (userId) {
-      // Fetch user data
       fetch(`${t("fetch")}utilisateurs/${userId}`)
         .then(response => response.json())
         .then(data => {
-          // Check if data.ville exists
           setUser({
             ...data,
             province_id: data.ville ? data.ville.province_id : ''
@@ -42,11 +45,9 @@ function FormFacture({ t, userId, totalPanier }) {
         });
     }
   
-    // Fetch villes data
     fetch(`${t("fetch")}villes`)
       .then(response => response.json())
       .then(data => {
-        // Ensure that item.nom can be parsed correctly
         const updatedData = data.map(item => ({
           ...item,
           nom: typeof item.nom === 'string' ? JSON.parse(item.nom) : item.nom
@@ -57,11 +58,9 @@ function FormFacture({ t, userId, totalPanier }) {
         console.error("There was an error fetching the villes!", error);
       });
   
-    // Fetch provinces data
     fetch(`${t("fetch")}provinces`)
       .then(response => response.json())
       .then(data => {
-        // Ensure that item.nom can be parsed correctly
         const updatedData = data.map(item => ({
           ...item,
           nom: typeof item.nom === 'string' ? JSON.parse(item.nom) : item.nom
@@ -72,11 +71,9 @@ function FormFacture({ t, userId, totalPanier }) {
         console.error("There was an error fetching the provinces!", error);
       });
   
-    // Fetch taxes data
     fetch(`${t("fetch")}taxes`)
       .then(response => response.json())
       .then(data => {
-        // Convert taux to a float value
         const updatedData = data.map(item => ({
           ...item,
           taux: parseFloat(item.taux) || 0
@@ -88,21 +85,19 @@ function FormFacture({ t, userId, totalPanier }) {
       });
   
   }, [userId, t]);
-  
 
   useEffect(() => {
     if (user.province_id) {
       const filtered = villes.filter(ville => ville.province_id === parseInt(user.province_id));
       setFilteredVilles(filtered);
 
-      // Calculer le total avec les taxes
       const tax = taxes.find(tax => tax.province_id === parseInt(user.province_id));
       if (tax) {
         const taxAmount = totalPanier * (tax.taux);
         const tauxTaxe = (tax.taux) * 100;
         setTotalWithTax(totalPanier + taxAmount);
-        setTaxName(tax.type); // recupérer le nom de la taxe
-        setTaxRate(tauxTaxe); // recupérer le taux de la taxe
+        setTaxName(tax.type);
+        setTaxRate(tauxTaxe);
       } else {
         setTotalWithTax(totalPanier);
         setTaxName('');
@@ -136,19 +131,16 @@ function FormFacture({ t, userId, totalPanier }) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    
-    // Perform form submission logic here
     console.log("Form submitted with total including tax:", totalWithTax);
   };
 
   return (
-    <div className="flex ">
-      <div className="w-full ">
-        <h2 className='text-center text-xl my-6'>Veuillez entrer vos coordonnées pour compléter l'achat</h2>
+    <div className="flex">
+      <div className="w-full">
+        <h2 className="text-center text-xl my-6">Veuillez entrer vos coordonnées pour compléter l'achat</h2>
 
         <div className="max-w-lg mx-auto bg-white p-8 rounded-md shadow-md">
-          
-          <form >
+          <form onSubmit={handleSubmit}>
             <div className="grid grid-cols-1 gap-6">
               <div>
                 <label className="block text-sm font-medium text-gray-700">{t("user.prenom")}</label>
@@ -236,9 +228,9 @@ function FormFacture({ t, userId, totalPanier }) {
                   name="province_id"
                   value={user.province_id}
                   onChange={handleChange}
-                  className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                  className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm rounded-md"
                 >
-                  <option value="">{t("user.selectprovince")}</option>
+                  <option value="">{t("selectionner")}</option>
                   {provinces.map(province => (
                     <option key={province.id} value={province.id}>
                       {province.nom[language]}
@@ -252,9 +244,9 @@ function FormFacture({ t, userId, totalPanier }) {
                   name="ville_id"
                   value={user.ville_id}
                   onChange={handleChange}
-                  className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                  className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm rounded-md"
                 >
-                  <option value="">{t("user.selectville")}</option>
+                  <option value="">{t("selectionner")}</option>
                   {filteredVilles.map(ville => (
                     <option key={ville.id} value={ville.id}>
                       {ville.nom[language]}
@@ -263,23 +255,97 @@ function FormFacture({ t, userId, totalPanier }) {
                 </select>
               </div>
             </div>
+
             <div className="text-right mt-8 mb-6">
-            <h3 className="text-lg font-medium text-gray-700">Total: <strong>{totalPanier} $</strong></h3>
-            <h3 className="text-lg font-medium text-gray-700 mt-6">Taxe: <strong>{taxName}</strong> : <strong>{tauxTaxe}%</strong></h3>
-            <h3 className="text-lg font-medium text-gray-700 mt-6">Total TTC : <strong>{totalWithTax} $</strong></h3>
-          </div>
-            <div className="mt-6 flex justify-end">
-              <button
-                type="submit"
-                className="px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out"
-              >
-                Payer
-              </button>
+              <h3 className="text-lg font-medium text-gray-700">Total: <strong>{totalPanier} $</strong></h3>
+              <h3 className="text-lg font-medium text-gray-700 mt-6">Taxe: <strong>{taxName}</strong> : <strong>{tauxTaxe}%</strong></h3>
+              <h3 className="text-lg font-medium text-gray-700 mt-6">Total TTC : <strong>{totalWithTax} $</strong></h3>
+            </div>
+
+            <div className="mt-6">
+              <Elements stripe={stripePromise}>
+                <CheckoutForm totalWithTax={totalWithTax} />
+              </Elements>
             </div>
           </form>
         </div>
       </div>
     </div>
+  );
+}
+
+function CheckoutForm({ totalWithTax }) {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);  // Ajoutez cet état pour suivre le succès du paiement
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    setIsProcessing(true);
+    setError(null);
+    setSuccess(null);  // Réinitialiser l'état de succès
+
+    const cardElement = elements.getElement(CardElement);
+
+    const { error: stripeError, paymentMethod } = await stripe.createPaymentMethod({
+      type: 'card',
+      card: cardElement,
+    });
+
+    if (stripeError) {
+      setError(stripeError.message);
+      setIsProcessing(false);
+      return;
+    }
+
+    try {
+      const response = await fetch('/api/stripe/payment', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          amount: Math.round(totalWithTax * 100), // Montant en centimes
+          currency: 'usd',
+          source: paymentMethod.id,
+          description: 'Achat de véhicule',
+        }),
+      });
+
+      const result = await response.json();
+
+      if (result.success) {
+        setSuccess('Paiement réussi!');  // Mettez à jour l'état de succès
+      } else {
+        setError('Erreur de paiement.');
+      }
+    } catch (error) {
+      setError('Erreur lors du traitement du paiement.');
+    }
+
+    setIsProcessing(false);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardElement />
+      {error && <div className="error">{error}</div>}
+      {success && <div className="success">{success}</div>} 
+      <button
+        type="submit"
+        disabled={isProcessing}
+        className="px-4 py-2 font-medium text-white bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:shadow-outline-blue active:bg-blue-600 transition duration-150 ease-in-out"
+      >
+        {isProcessing ? 'Processing...' : 'Payer'}
+      </button>
+    </form>
   );
 }
 
